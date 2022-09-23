60e4e376eda3c5a4501f0cc9cbbd1ebd0069a5a6
Raul C <raul.gines1999@gmail.com>
2022-09-22 22:42:18 -0500

    añadiendo responsive en el menu

--- src/components/pokedex/NavMenu.jsx
+++ src/components/pokedex/NavMenu.jsx
@@ -24,12 +24,20 @@ const NavMenu = () => {
             <div className="navbar__header-content flex">
                <a href="#" className='navbar__logo'><img src={Logo} alt="Logo pokemon" /></a>
 
+               <nav className="menu-content-desktop">
+                  <ul className='menu-content-desktop__ul flex'>
+                     <li onClick={clickBtnMenu} className='menu-content-desktop__item'><NavLink to={'/'} >Home</NavLink></li>
+                     <li onClick={clickBtnMenu} className='menu-content-desktop__item'><NavLink to={'/pokedex'} >Pokedex</NavLink></li>
+                  </ul>
+               </nav>
+
                <button onClick={clickBtnMenu} className="navbar-btn">
                   <i className='bx bx-menu'></i>
                </button>
             </div>
 
             <div className={`menu-box ${!btnMenu ? " menu-box-hidden" : ""}`}>
+
                <div className="menu-content">
                   <div onClick={clickBtnMenu} className="btn-close flex">
                      <button><i className='bx bx-x'></i></button>
@@ -42,15 +50,13 @@ const NavMenu = () => {
                      <ul className='menu-content__ul flex'>
                         <li onClick={clickBtnMenu} className='menu-content__item'><NavLink to={'/'} >Home</NavLink></li>
                         <li onClick={clickBtnMenu} className='menu-content__item'><NavLink to={'/pokedex'} >Pokedex</NavLink></li>
-                        <li className='menu-content__item'><a href="#">Legendaries</a></li>
-                        <li className='menu-content__item'><a href="#">Algo mas</a></li>
                      </ul>
                   </div>
                </div>
             </div>
          </nav>
 
-         
+
 
       </header>
    )
